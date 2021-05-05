b9789d94c2c5cfb4440fec12d12a2d644c709f79
gorvardanyan1 <gorv887@gmail.com>
2021-05-05 18:06:50 +0400

    my first repasitori

---
+++ game.js
@@ -0,0 +1,355 @@
+  
+var answerOne  = [
+ {
+    answer: "Քանի՞ վարչատարածքային միավորից է կազմված Հայաստանի Հանրապետությունը",
+    option1: "7",
+    option2: "12",
+    option3: "11",
+    option4: "14",
+    question: "11"
+},
+ {
+  answer: "Ո՞ր թվականին է Մեսրոպ Մաշտոցը ստեղծել հայկական գրային համակարգը(Հայերենի այբուբենը)",
+  option1: "405թ.",
+  option2: "387թ.",
+  option3: "Ք.ա. 95թ.",
+  option4: "389-414թ.թ",
+  question: "405թ."
+ },
+ {
+  answer: "Ո՞րն է Իրանի մայրաքաղաքը",
+  option1: "Մոսկվա",
+  option2: "Թեհրան",
+  option3: "Անկարա",
+  option4: "Իսլամաբադ",
+  question: "Թեհրան"
+ },
+ {
+   answer: "Ո՞ր թվականներին է երկիրը կառավարել Տիգրան II Մեծ արքան",
+   option1: "Ք.ա.189-Ք.ա.160թ.թ.",
+   option2: "Ք.ա.55-Ք.ա.34թ.թ.",
+   option3: "350-368թ.թ.",
+   option4: "Ք.ա.95-Ք.ա.55թ.թ.",
+   question: "Ք.ա.95-Ք.ա.55թ.թ."
+ },
+  {
+    answer: "Երբ է հռչակվել Հայաստանի առաջին հանրապետությունը",
+    option1: "1920թ դեկտեմբերի 2",
+    option2: "1918թ մայիսի 28",
+    option3: "1921թ ապրիլի 5",
+    option4: "1918թ նոյեմբերի 28",
+    question: "1918թ մայիսի 28"
+  },
+  {
+    answer: "Ո՞րն է առաջին հայ ազգային կուսակցությունը",
+    option1: "Արմենական",
+    option2: "Հայ հեղափոխական դաշնակցություն",
+    option3: "Հնչակյան",
+    option4: "Հայոց համազգային շարժում",
+    question: "Արմենական",
+  },
+  {
+    answer: "Ո՞վ էր Հայաստանի առաջին հանրապետության վարչապետը",
+    option1: "Ավետիս Ահարոնյան",
+    option2: "Հովհաննես Քաջազնունի",
+    option3: "Գարեգին Նժդեհ",
+    option4: "Մովսես Սիլիկյան",
+    question: "Հովհաննես Քաջազնունի"
+  },
+  {
+  answer: "Ո՞ր քաղաքում է թագավոր օծվել Լևոն Մեծագործը",
+  option1: "Ադանա",
+  option2: "Մասիս",
+  option3: "Սիս",
+  option4: "Տարսոն",
+  question: "Տարսոն"
+  },
+  {
+    answer: "Ով է 1918 թ. ի մարտի 24-ին հաստատվել Երևանի նահանգի դիկատոր",
+    option1: "Արամ Մանուկյան",
+    option2: "Դանիել Բեկ-Փիրումյան",
+    option3: "Ալեքասանդր Խատիսյան",
+    option4: "Անդրանիկ Օզանյան",
+    question: "Արամ Մանուկյան"
+  },
+  {
+    answer: "<<Անուշ>> պոեմում ո՞վ է Անուշի եղբայրը",
+    option1: "Սարո",
+    option2: "Թուսյան",
+    option3: "Մոսի",
+    option4: "Գարեգին",
+    question: "Մոսի"
+  },
+  {
+    answer: "Ե՞րբ են հայկական ուժերը ազատագրել Շուշին",
+    option1: "1994թ ապրիլի 9",
+    option2: "1990թ հունիսի 10",
+    option3: "1992թ մայիսի 9",
+    option4: "1993թ մայիսի 9",
+    question: "1992թ մայիսի 9"
+    },
+    {
+      answer: "Ո՞րն է Գևորգ Չավուշի ծննդավայրը",
+      option1: "Սոխորդ գյուղ",
+      option2: "Շապին Կարահիսար քաղաք",
+      option3: "Մկդենք գյուղ",
+      option4: "Ահարոնք գյսւղ",
+      question: "Մկդենք գյուղ"
+    },
+    {
+      answer: "Ո՞վ է այս խոսքերի հեղինակը.«Եթե Ղարաբաղը կործանվի, ապա միայն քաղաքական խաղերի պատճառով է կործանվելու, քաղաքականությունն ամենասարսափելի գիշատիչն է»",
+      option1: "Վազգեն Սարգսյան",
+      option2: "Վարդան Ստեփանյան",
+      option3: "Աշոտ Ղուլյան",
+      option4: "Լեոնիդ Ազգալդյան",
+      question: "Աշոտ Ղուլյան"
+      },
+      {
+       answer: "Ո՞ր ստեղծագործությունը չի գրել Գրիգոր Զոհրապը",
+       option1: "Ճիտին Պարտքը",
+       option2: "Զաբուղոն",
+       option3: "Երջանիկ Մահը",
+       option4: "Նեղ օրերից մեկը",
+       question: "Նեղ օրերից մեկը"
+      },
+  {
+   answer: "<<Հին օրերի երգը>> ֆիլմում քանի՞ որդի ուներ Հայաստան մայրիկը։",
+   option1: "2",
+   option2: "7",
+   option3: "3",
+   option4: "4",
+   question: "4"
+   },
+];
+var answerTwo = [
+  {
+    answer: "Ե՞րբ է Քրիստոնեությունը Հայաստտանում ընդունվում պետական կրոն",
+    option1: "405թ",
+    option2: "305թ",
+    option3: "301թ",
+    option4: "387թ",
+    question: "301թ"
+  },
+  {
+    answer: "Ո՞րն է Ամերիկայի Միացյալ Նահանգների մայրաքաղաքը",
+    option1: "Փարիզ",
+    option2: "Մոսկվա",
+    option3: "Վաշինգտոն",
+    option4: "Կանբերրա",
+    question: "Վաշինգտոն"
+  },
+  {
+    answer: "Ո՞վ է ստեղծել Facebook սոցիալական ցանցը",
+    option1: "Իլոն Մասկ",
+    option2: "Մարկ Ցուկերբերգ",
+    option3: "Սթիվ Ջոբս",
+    option4: "Բիլ Գեյթս",
+    question: "Մարկ Ցուկերբերգ"
+  },
+  {
+    answer: "Ո՞վ է Աղբյուր Սերոբը",
+    option1: "Հայդուկ",
+    option2: "հոգևորական",
+    option3: "դիվանագետ",
+    option4: "պատգամավոր",
+    question: "Հայդուկ"
+  },
+  {
+    answer: "Ո՞ր երկրում է ձևավորվել ֆաշիզմը",
+    option1: "Գերմանիա",
+    option2: "Իտալիա",
+    option3: "ԱՄՆ",
+    option4: "Ռուսաստան",
+    question: "Իտալիա"
+  },
+  {
+    answer: "Ե՞րբ է ստեղծվել Գերմանիայի Դաշնային Հանրապետությունը",
+    option1: "1950 հունվար",
+    option2: "1948թ սեպտեմբեր",
+    option3: "1949թ սեպտեմբեր",
+    option4: "1952թ",
+    question: "1949թ սեպտեմբեր"
+  },
+  {
+    answer: "Բագրատունի, ո՞ր արքան է Անին դարձրել մայրաքաղաք.",
+    option1: "Գագիկ II",
+    option2: "Աշոտ III",
+    option3: "Աշոտ II",
+    option4: "Աբաս I",
+    question: "Աշոտ III"
+  },
+  {
+    answer: "Ո՞ր պատմիչն է նկարագրել Վահանանց ապստամբությունը.",
+    option1: "Ղազար Փարպեցի",
+    option2: "Մովսես Խորենացի",
+    option3: "Եղիշե",
+    option4: "Փավստոս Բուզանդ",
+    question: "Ղազար Փարպեցի"
+  },
+  {
+    answer: "Հայ Առաքելական Եկեղեցու ներկայիս առաջնորդ Ամենայն Հայոց Գարեգին Բ կաթողիկոսը թվով որերո՞րդ կաթողիկոսն է",
+    option1: "135-րդը",
+    option2: "130-րդը",
+    option3: "132-րդը",
+    option4: "129-րդը",
+    question: "132-րդը"
+  },
+  {
+    answer: "Հայ ո՞ր թագավորն է եղել Մադրիդի տիրակալ",
+    option1: "Թորոս Ա",
+    option2: "Ռուբեն Գ",
+    option3: "Հեթում Բ",
+    option4: "Լևոն Զ",
+    question: "Լևոն Զ"
+  },
+  {
+    answer: "Ի՞նչ էր հանճարեղ բյուզանդական հայ սպարապետի անունը, ով գրավել է Հռոմը",
+    option1: "Նարսես",
+    option2: "Բելիսարիոս",
+    option3: "Վիտալիանոս",
+    option4: "Նիկեփորաս Փոկաս",
+    question: "Նարսես"
+  },
+  {
+    answer: "Հայաստանի ո՞ր տարածքն էր որդան կարմիր հռչակավոր ներկի արտադրության կենտրոնը.",
+    option1: "Անի",
+    option2: "Դվին",
+    option3: "Կարս",
+    option4: "Վանն",
+    question: "Դվին"
+  },
+  {
+    answer: "Ում բառերն են. «Իմ այնքան ծառայությունների, արյուն ու քրտինք թափելու… հատուցումն ա՞յս եղավ: Բայց այս մահը երանի՜ թե ձիու վրա ինձ հասներ»",
+    option1: "Սամվել Մամիկոնյան",
+    option2: "Վահան Մամիկոնյան",
+    option3: "Վասակ Մամիկոնյան",
+    option4: "Մուշեղ Մամիկոնյան",
+    question: "Մուշեղ Մամիկոնյան"
+  },
+  {
+    answer: "Ո՞ր գետն է հոսում ԱՄՆ-ի Գրանդ Կանյոնով",
+    option1: "Միսիսիպի",
+    option2: "Ալաբամա",
+    option3: "Կոլորադո",
+    option4: "Թենեսի",
+    question: "Կոլորադո"
+  },
+  {
+    answer: "Հարսնացուն հյուսիսից ֆիլմում ի՞նչ էր փեսացուի՝ Արտակի հոր անունը",
+    option1: "Արշակ",
+    option2: "Մուրադ",
+    option3: "Վազգեն",
+    option4: "Պետրոս",
+    question: "Մուրադ"
+  },
+]
+ var arr = [answerOne, answerTwo]
+var random = Math.floor(Math.random() * (2 - 0) + 0)
+var answer1 = arr[random]
+var answerid = document.getElementById("answer")
+var questionClass = document.querySelectorAll(".question1")
+var many = document.querySelectorAll(".milionmany")
+var phone = document.getElementById("phone")
+var auditor = document.getElementById("auditor")
+var five = document.getElementById("five")
+var sos = document.getElementById("sos")
+answerid.innerHTML =  answer1[0].answer  
+questionClass[0].innerHTML = answer1[0].option1
+questionClass[1].innerHTML = answer1[0].option2
+questionClass[2].innerHTML = answer1[0].option3
+questionClass[3].innerHTML = answer1[0].option4
+many[15].style.color = "#FF0051"
+var a = 0
+var b = 15
+for(var u = 0; u < 4; u++){
+questionClass[u].addEventListener("click" , function(){
+  sos.innerHTML = ""
+  if(this.innerHTML == answer1[a].question){
+    setTimeout(function(){
+      
+    
+     a += 1 
+    if(a > 14){
+      answerid.innerHTML = "Խաղն ավարտվեց դու հաղթեցիր"
+      $(".question1").hide(2000);
+      $("#answer").animate({
+        "font-size": "70px"
+      }, 2000);
+    many[0].style.color = "#E97C00"
+    }
+    answerid.innerHTML =  answer1[a].answer 
+questionClass[0].innerHTML = answer1[a].option1
+questionClass[1].innerHTML = answer1[a].option2
+questionClass[2].innerHTML = answer1[a].option3
+questionClass[3].innerHTML = answer1[a].option4
+ b -= 1
+ many[b].style.color = "#FF0051"
+}, 300)
+ phone.addEventListener("click" , function(){
+   sos.innerHTML = `Ընկերոջ պատասխանն է ${answer1[a].question}`
+     $("#phone").hide(5000);
+  
+ })
+ auditor.addEventListener("click", function(){
+  sos.innerHTML = `Դահլիճի մեծամասնության կարծիքով ճիշտ պատասխանն է ${answer1[a].question}`
+  $("#auditor").hide(5000);
+})
+five.addEventListener("click", function(){
+  var arr = []
+  for(var i = 0 ; i < 4; i++){
+    if(questionClass[i].innerHTML == answer1[a].question){
+      var q1 = answer1[a].option1
+      var q2 = answer1[a].option2
+      var q3 = answer1[a].option3
+      var q4 = answer1[a].option4
+      arr.push(q1)
+      arr.push(q2)
+      arr.push(q3)
+      arr.push(q4)
+    questionClass[0].innerHTML = ""
+    questionClass[1].innerHTML = ""
+    questionClass[2].innerHTML = ""
+    questionClass[3].innerHTML = "" 
+    questionClass[i].innerHTML = answer1[a].question
+    
+    var rand = Math.floor(Math.random() * (4  - 0) + 0)
+  
+    if(rand == i){
+      rand = Math.floor(Math.random() * (4  - 0) + 0)
+    }
+   questionClass[rand].innerHTML = arr[rand]
+    }
+  }
+  $("#five").hide(5000);
+})
+
+  }
+  else if(this.innerHTML != answer1[a].question){
+    answerid.innerHTML = "Խաղն ավարտվեց Դու պարտվեցիր"
+  $(".question1").hide(2000);
+  $("#answer").animate({
+    "font-size": "70px"
+  }, 2000);
+  }
+  
+})
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
